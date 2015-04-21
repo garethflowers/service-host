5f33d145517470f51e1123c6efe46ee65ddc175b
Gareth Flowers <garethflowers@users.noreply.github.com>
2015-04-21 22:05:20 +0100

    Added initial ServiceHost project

---
+++ ServiceHost/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ServiceHost
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        private static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                return;
+            }
+
+            System.ServiceProcess.ServiceBase[] ServicesToRun;
+
+            ServicesToRun = new System.ServiceProcess.ServiceBase[]
+			{
+				new ServiceAgent(args)
+			};
+
+            System.ServiceProcess.ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
---
+++ ServiceHost/ServiceAgent.cs
@@ -0,0 +1,50 @@
+﻿namespace ServiceHost
+{
+    public partial class ServiceAgent : System.ServiceProcess.ServiceBase
+    {
+        private readonly string _arguments;
+        private readonly string _command;
+
+        private System.Diagnostics.Process _process;
+
+        public ServiceAgent()
+        {
+            this.InitializeComponent();
+        }
+
+        public ServiceAgent(string[] args)
+        {
+            this._command = args[0];
+
+            for (int i = 1; i < args.Length; i++)
+            {
+                this._arguments += ' ' + args[i];
+            }
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            _process = new System.Diagnostics.Process();
+            _process.StartInfo.FileName = this._command;
+            _process.StartInfo.Arguments = this._arguments;
+            _process.Start();
+        }
+
+        protected override void OnStop()
+        {
+            if (this._process == null || this._process.HasExited)
+            {
+                return;
+            }
+
+            try
+            {
+                this._process.Kill();
+            }
+            finally
+            {
+                this._process = null;
+            }
+        }
+    }
+}
